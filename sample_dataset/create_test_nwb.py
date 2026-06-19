6be18c3f0f997d007ef4b53e5b4b5172b25dab2e
Alessio Buccino <alessio.buccino@alleninstitute.org>
2026-06-19 07:30:45 -0600

    Prepare 1.2.4 release (#114)

--- sample_dataset/create_test_nwb.py
+++ sample_dataset/create_test_nwb.py
@@ -17,6 +17,8 @@ from neuroconv.tools.spikeinterface import add_recording_to_nwbfile
 
 this_folder = Path(__file__).parent
 
+si.set_global_job_kwargs(n_jobs=0.7)
+
 def generate_nwb():
     duration = 180
     short_duration = 10
@@ -25,7 +27,7 @@ def generate_nwb():
     output_folder = this_folder / "nwb"
     output_folder.mkdir(exist_ok=True)
 
-    recording, _ = si.generate_ground_truth_recording(
+    recording_main, _ = si.generate_ground_truth_recording(
         num_channels=num_channels,
         num_units=num_units,
         durations=[duration],
@@ -33,10 +35,15 @@ def generate_nwb():
 
     nwbfile = mock_NWBFile()
     nwbfile.subject = mock_Subject()
-    add_recording_to_nwbfile(recording, nwbfile=nwbfile)
+    metadata = dict(Ecephys=dict())
+    metadata['Ecephys']['ElectricalSeriesMain'] = dict(
+        name="main",
+        description="Main recording"
+    )
+    add_recording_to_nwbfile(recording_main, nwbfile=nwbfile, metadata=metadata, es_key="ElectricalSeriesMain")
 
     # Also add one short recording that should be skipped
-    short_recording, _ = si.generate_ground_truth_recording(
+    recording_short, _ = si.generate_ground_truth_recording(
         num_channels=num_channels,
         num_units=num_units,
         durations=[short_duration],
@@ -46,7 +53,27 @@ def generate_nwb():
         name="short",
         description="Short recording"
     )
-    add_recording_to_nwbfile(short_recording, nwbfile=nwbfile, metadata=metadata, es_key="ElectricalSeriesShort")
+    add_recording_to_nwbfile(recording_short, nwbfile=nwbfile, metadata=metadata, es_key="ElectricalSeriesShort")
+
+    # Add unsigned electrical series
+    recording, _ = si.generate_ground_truth_recording(
+        num_channels=num_channels,
+        num_units=num_units,
+        durations=[duration],
+    )
+    traces = recording.get_traces() 
+    # add offset
+    traces_unsigned = traces + 2**15
+    traces_unsigned = traces_unsigned.astype('uint16')
+    recording_unsigned = si.NumpyRecording(traces_unsigned, sampling_frequency=recording.get_sampling_frequency())
+    recording_unsigned.set_probe(recording.get_probe(), in_place=True)
+    recording_unsigned.set_channel_gains(1)
+    recording_unsigned.set_channel_offsets(0)
+    metadata['Ecephys']['ElectricalSeriesUnsigned'] = dict(
+        name="unsigned",
+        description="Unsigned recording"
+    )
+    add_recording_to_nwbfile(recording_unsigned, nwbfile=nwbfile, metadata=metadata, es_key="ElectricalSeriesUnsigned")
 
     with NWBHDF5IO(output_folder / "sample.nwb", mode="w") as io:
         io.write(nwbfile)
@@ -54,7 +81,8 @@ def generate_nwb():
     # save spikeinterface recording zarr format for testing the job dispatch
     output_si_folder = this_folder / "spikeinterface"
     output_si_folder.mkdir(exist_ok=True)
-    recording.save(folder=output_si_folder / "sample_recording.zarr", format="zarr")
+    for recording_name, recording in zip(["main", "short", "unsigned"], [recording_main, recording_short, recording_unsigned]):
+        recording.save(folder=output_si_folder / f"sample_recording_{recording_name}.zarr", format="zarr", overwrite=True)
 
 if __name__ == '__main__':
     generate_nwb()
